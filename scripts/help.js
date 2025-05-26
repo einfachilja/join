20cd5ebd5725957a74a085ed7c3d163636557be6
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-05-26 21:04:45 +0200

    arrow left in help page works now

---
+++ scripts/help.js
@@ -0,0 +1,12 @@
+function returnToPrevPage(){
+    // Check if there is a previous page in the history
+    if (document.referrer && !document.referrer.includes('help.html')) {
+        // Redirect to the previous page
+        window.location.href = document.referrer;
+    }
+    // If there is no referrer, redirect to a default page
+    else {
+        window.location.href = './summary.html';
+    }
+    return false;
+}
